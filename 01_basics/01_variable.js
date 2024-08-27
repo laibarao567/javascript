7727f60dfae3203db808a33fadcbf5d9f495936b
laibarao567 <laibarao449@gmail.com>
2024-08-27 09:18:18 +0000

    variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId=14445
+let accountEmail= "laibarao@google.com"
+var accountPassword="12345"
+accountCity="sargodha"
+let accountState;
+
+accountEmail="hc@hc.com"
+accountPassword="212121"
+accountCity="lahore"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
